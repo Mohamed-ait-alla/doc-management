09f428d357061075c60f797479355553a8936f8e
Mohamed-ait-alla <mohamedmhdo2000@gmail.com>
2025-10-15 22:19:08 +0100

    feat(front-end): add a search feild (creation & styling)

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-import Header from "./components/header";
+import Header from "./components/Header";
+import Search from "./components/Search";
 
 export default function Home() {
   return (
     <div>
      <Header />
+
+     <div className="body-container">
+      <div className="search-container">
+        <Search />
+      </div>
+
+     </div>
     </div>
   );
 }
